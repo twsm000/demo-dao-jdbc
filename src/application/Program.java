66acf4d834ff75aea7c0a439f1c801db284afc16
Thomas M <twsm000@gmail.com>
2020-05-21 07:57:52 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,8 +13,11 @@ public class Program {
    
        Department department = new Department(1, "Books");
        System.out.println(department);
-       
+
        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, department);
        System.out.println(seller);
+       
+       SellerDao sellerDao = DaoFactory.createSellerDao();
+       System.out.println(sellerDao);
    } 
 }
