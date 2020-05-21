95d12b15e6743a19033270466dc3a99522220e48
Thomas M <twsm000@gmail.com>
2020-05-20 21:53:57 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
    
@@ -8,5 +11,8 @@ public class Program {
    
        Department department = new Department(1, "Books");
        System.out.println(department);
+       
+       Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, department);
+       System.out.println(seller);
    } 
 }
